ce7b261bbae775c7d43baac4787cd099deef9633
Just-Lulu <125663941+Just-Lulu@users.noreply.github.com>
2024-08-21 23:03:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 // src/App.js
 import React from 'react';
 import TodoList from './TodoList';
-import SpeedInsights from '@vercel/speed-insights/react';
-
 
 function App() {
   return (
     <div>
       <TodoList />
-      <SpeedInsights />
-
     </div>
   );
 }
